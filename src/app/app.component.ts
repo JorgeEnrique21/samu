45213fbb50cb7b386b7e9462960dcf5efd32f35d
JorgeEnrique21 <jorgenrique_azevedo@yahoo.com.br>
2017-07-10 12:47:11 -0300

    Finalizado (Ao que tudo indica)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,9 +15,11 @@ import {UFs} from './services/mock-ufs';
   providers: [UFService, SamuService]
 })
 export class AppComponent implements OnInit {
-    title = 'Detalhes da SAMU, maravilhoso, né?';
+    title = 'Detalhes do serviço da SAMU, maravilhoso, né?';
     ufs : UF[];
     uf: UF;
+    qtd: number;
+    media: number;
     dados: Dados[];
     dados_da_samu : Dados[];
     constructor(private ufService: UFService, private samuService: SamuService)
@@ -28,5 +30,6 @@ export class AppComponent implements OnInit {
         this.dados_da_samu = this.samuService.getAllMunicipiosAtendidosPorEstado();
         this.uf = this.ufService.encontrarUF(23);
         this.dados = this.samuService.encontrarSAMU(23);
+        this.media = this.samuService.mediaMunicipio(23);
     }
 }
