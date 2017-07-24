c6506f2db30b62f2fbb06470324e7183a2d213d3
JorgeEnrique21 <jorgenrique_azevedo@yahoo.com.br>
2017-07-24 09:57:00 -0300

    Correcões topper, pronto para sofrer mais

--- src/app/todos-dados.component.ts
+++ src/app/todos-dados.component.ts
@@ -2,10 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 import {UF} from './types/uf';
 import {UFService} from './services/uf.service'
-
+import {DadoNome} from "./types/todasufs";
 import {Dados} from './types/samu';
 import {SamuService} from './services/samu.service'
-
+import {TodasUFsService} from "./services/todasufs.service";
 import {UFs} from './services/mock-ufs';
 
 @Component({
@@ -16,21 +16,21 @@ import {UFs} from './services/mock-ufs';
 })
 
 export class TodosDadosComponent implements OnInit {
-    title = 'Todos os dados do maravilhoso serviço da SAMU';
+    title = 'Todos os dados do maravilhoso serviço da SAMU em todos os estados desse Brasilzão maravilhoso';
     ufs : UF[];
     uf: UF;
     qtd: number;
     media: number;
-    dados: Dados[];
+    dados: DadoNome[];
     dados_da_samu : Dados[];
-    constructor(private ufService: UFService, private samuService: SamuService)
+    constructor(private ufService: UFService, private samuService: SamuService, private TodasUFsService: TodasUFsService)
     { }
 
     ngOnInit(): void {
         this.ufs = this.ufService.getAll();
         this.dados_da_samu = this.samuService.getAllMunicipiosAtendidosPorEstado();
         this.uf = this.ufService.getPorID(23);
-        this.dados = this.samuService.getPorUFMunicipiosAtendidosPorEstado(this.uf);
         this.media = this.samuService.mediaMunicipio(23);
+        this.dados = this.TodasUFsService.mesclardados();
     }
 }
